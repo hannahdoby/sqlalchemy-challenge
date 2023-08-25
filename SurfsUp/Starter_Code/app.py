d49734c266c5b0f4d37af5a9bba7105cfbd7ad67
hannahdoby <hannahdoby@outlook.com>
2023-08-24 21:34:11 -0400

    final commit

--- SurfsUp/Starter_Code/app.py
+++ SurfsUp/Starter_Code/app.py
@@ -1,30 +1,107 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, distinct
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return a list of all precipitation values"""
+    # Query all precipitation values
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    # Convert list of tuples into normal list
+    all_precipitation = list(np.ravel(results))
+
+    return jsonify(all_precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of all station names"""
+    # Query all station names
+    results = session.query(Station.station).all()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list of all temperature observations for the previous year"""
+    # Query all temperature observations for the previous year
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23').all()
+
+    # Convert list of tuples into normal list
+    all_tobs = list(np.ravel(results))
+
+    return jsonify(all_tobs)
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    """Return a list of the minimum, average and maximum temperature for a given start date"""
+    # Query the minimum, average and maximum temperature for a given start date
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+
+    # Convert list of tuples into normal list
+    start_tobs = list(np.ravel(results))
+
+    return jsonify(start_tobs)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    """Return a list of the minimum, average and maximum temperature for a given start and end date"""
+    # Query the minimum, average and maximum temperature for a given start and end date
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start)\
+        .filter(Measurement.date <= end).all()
+
+    # Convert list of tuples into normal list
+    start_end_tobs = list(np.ravel(results))
+
+    return jsonify(start_end_tobs)
+
+if __name__ == '__main__':
+    app.run(debug=True)
